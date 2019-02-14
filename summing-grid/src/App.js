d65e8689925d03affc5dc5c588ee5721bc02617d
Steven Frieson <sfrieson@gmail.com>
2019-02-13 19:22:45 -0500

    Supports four cells at brekpoints

--- summing-grid/src/App.js
+++ summing-grid/src/App.js
@@ -2,11 +2,17 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-  render() {
+  render () {
     return (
-      <div className="App">
-        <p className="App-intro">
+      <div className='App'>
+        <p className='App-intro'>
           To get started, edit <code>src/App.js</code> and save to reload.
+          <div className='grid'>
+            <div className='grid-cell'>1</div>
+            <div className='grid-cell'>2</div>
+            <div className='grid-cell'>3</div>
+            <div className='grid-cell'>4</div>
+          </div>
         </p>
       </div>
     );
