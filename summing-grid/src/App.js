dbe568aed401c71e9d0a99332daf12db3769f0f1
Steven Frieson <sfrieson@gmail.com>
2019-02-13 19:36:10 -0500

    Adds three inputs

--- summing-grid/src/App.js
+++ summing-grid/src/App.js
@@ -1,17 +1,51 @@
 import React, { Component } from 'react';
 import './App.css';
 
+const inputs = [
+  {name: 'cell1', label: 'Cell 1'},
+  {name: 'cell2', label: 'Cell 2'},
+  {name: 'cell3', label: 'Cell 3'}
+];
+
 class App extends Component {
+  constructor (props) {
+    super(props);
+
+    this.state = inputs.reduce((state, input) => {
+      state[input.name] = '';
+      return state;
+    }, {});
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange ({ target }) {
+    this.setState({[target.name]: target.value});
+  }
+
   render () {
+    const sum = 4;
+
     return (
       <div className='App'>
         <p className='App-intro'>
           To get started, edit <code>src/App.js</code> and save to reload.
           <div className='grid'>
-            <div className='grid-cell'>1</div>
-            <div className='grid-cell'>2</div>
-            <div className='grid-cell'>3</div>
-            <div className='grid-cell'>4</div>
+            {inputs.map(input => (
+              <div className='grid-cell'>
+                <label className='visually-hidden' htmlFor={input.name}>
+                  {input.label}
+                </label>
+                <input id={input.name}
+                  className='grid-cell__input number-display'
+                  name={input.name}
+                  onChange={this.handleChange}
+                  type='text'
+                  value={this.state[input.name]}
+                />
+              </div>
+            ))}
+            <div className='grid-cell number-display'>{sum}</div>
           </div>
         </p>
       </div>
